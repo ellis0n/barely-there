c68eccb09975c696c75fb0fe4264e80a09238ba0
ellis0n <100211236+ellis0n@users.noreply.github.com>
2024-03-23 20:20:00 -0230

    init

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,64 +1,64 @@
 import { defineCollection, reference, z } from "astro:content";
 
 const posts = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		username: reference('authors'),
-		relatedPosts: z.array(reference("posts")),
-		draft: z.boolean().optional(),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    username: reference("authors"),
+    relatedPosts: z.array(reference("posts")),
+    draft: z.boolean().optional(),
+  }),
 });
 
 const authors = defineCollection({
-	type: "content",
-	schema: z.object({
-		username: z.string(),
-		firstname: z.string(),
-		lastname: z.string(),
-		email: z.string(),
-		github: z.string().url().optional(),
-		twitter: z.string().url(),
-		instagram: z.string().url(),
-		avatar: z.string(),
-		authoredPosts: z.array(reference("posts")),
-	}),
+  type: "content",
+  schema: z.object({
+    username: z.string(),
+    firstname: z.string(),
+    lastname: z.string(),
+    email: z.string(),
+    github: z.string().url().optional(),
+    twitter: z.string().url(),
+    instagram: z.string().url(),
+    avatar: z.string(),
+    authoredPosts: z.array(reference("posts")),
+  }),
 });
 
 const releases = defineCollection({
-	type: "content",
-	schema: ({ image }) =>
-		z.object({
-			artist: z.string(),
-			albumTitle: z.string(),
-			artwork: image(),
-			release: z.date(),
-			id: z.string(),
-			tags: z.string(),
-			video: z.string().url().optional(),
-			draft: z.boolean().optional(),
-			bandcamp: z.string().url().optional(),
-			bandcampEmbed: z.string().url().optional(),
-			spotify: z.string().url().optional(),
-			appleMusic: z.string().url().optional(),
-			soundcloud: z.string().url().optional(),
-			youtube: z.string().url().optional(),
-		}),
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      artist: z.string(),
+      albumTitle: z.string(),
+      artwork: image(),
+      release: z.date(),
+      id: z.string(),
+      tags: z.string(),
+      video: z.string().url().optional(),
+      draft: z.boolean().optional(),
+      bandcamp: z.string().url().optional(),
+      bandcampEmbed: z.string().url().optional(),
+      spotify: z.string().url().optional(),
+      appleMusic: z.string().url().optional(),
+      soundcloud: z.string().url().optional(),
+      youtube: z.string().url().optional(),
+    }),
 });
 
 const shows = defineCollection({
-	type: "content",
-	schema: ({ image }) =>
-		z.object({
-			featured: z.boolean().optional(),
-			title: z.string(),
-			venue: z.string().optional(),
-			date: z.date(),
-			poster: image(),
-			draft: z.boolean().optional(),
-			tickets: z.string().url().optional(),
-			tags: z.string().optional(),
-		}),
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+	featured: z.boolean().optional(),
+	title: z.string(),
+	venue: z.string().optional(),
+	date: z.date(),
+	poster: image(),
+	draft: z.boolean().optional(),
+	tickets: z.string().url().optional(),
+	tags: z.string().optional(),
+    }),
 });
 
 export const collections = { posts, authors, releases, shows };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,52 +1,59 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const plugin = require('tailwindcss/plugin');
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
-				mono: ["Geist Mono", ...defaultTheme.fontFamily.serif],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+				'100': '#CB3E1E',
+				'200': '#FEFEFE',
 			},
-			gridTemplateColumns: {
-				0: 'repeat(1,minmax(0, 0fr))',
-				'5e-5e-1f': 'minmax(0, 5em) minmax(0, 5em) minmax(0, 1fr)',
-				'header': '6vw 1fr 1fr 6vw',
+			gradientColorStopPositions: {
+				33: '33%',
 			},
-			gridTemplateRows: {
-				0: 'repeat(1,minmax(0, 0fr))',
-				footer: '3vw 1fr 1fr 3vw',
-			},
-			width: {
-				content: 'min(100vw - 1.5rem, 72rem)',
-				narrow: 'min(100vw - 1.5rem, 50rem)',
-			},
-			fontSize: {
-			100: '0.7025rem',
-			200: '0.79rem',
-			300: '0.88875rem',
-			400: '1rem',
-			500: '1.125rem',
-			600: '1.265625rem',
-			700: '1.42375rem',
-			800: '1.601875rem',
-			900: '1.801875rem',
-			1000: '2.0275rem',
-			1100: '2.280625rem',
-			1200: '2.565625rem',
-			1300: '2.8875rem',
-			1400: '3.25rem',
-		},
-		},
-	},
-	plugins:
-		[
-			plugin(function ({ addVariant }) {
-				addVariant('scroll', ':merge(.scroll) &');
-				addVariant('is-checked', ':merge(:checked +) &');
-			}), 
-			require("@tailwindcss/typography")
-		]
+      fontFamily: {
+        sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
+        mono: ["Geist Mono", ...defaultTheme.fontFamily.serif],
+      },
+      gridTemplateColumns: {
+        0: "repeat(1,minmax(0, 0fr))",
+        "5e-5e-1f": "minmax(0, 5em) minmax(0, 5em) minmax(0, 1fr)",
+        header: 'minmax(0, 100dvw) 1fr 2fr',
+
+      },
+      gridTemplateRows: {
+        0: "repeat(1,minmax(0, 0fr))",
+        footer: "3vw 1fr 1fr 3vw",
+      },
+      width: {
+        content: "min(100vw - 1.5rem, 72rem)",
+        narrow: "min(100vw - 1.5rem, 50rem)",
+      },
+      fontSize: {
+        100: "0.7025rem",
+        200: "0.79rem",
+        300: "0.88875rem",
+        400: "1rem",
+        500: "1.125rem",
+        600: "1.265625rem",
+        700: "1.42375rem",
+        800: "1.601875rem",
+        900: "1.801875rem",
+        1000: "2.0275rem",
+        1100: "2.280625rem",
+        1200: "2.565625rem",
+        1300: "2.8875rem",
+        1400: "3.25rem",
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("scroll", ":merge(.scroll) &");
+      addVariant("is-checked", ":merge(:checked +) &");
+    }),
+    require("@tailwindcss/typography"),
+  ],
 };
