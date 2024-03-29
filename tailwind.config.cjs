afdbf0853148a5b70a79f85e9fdb46b23e063801
ellis0n <100211236+ellis0n@users.noreply.github.com>
2024-03-28 21:53:54 -0230

    more work

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
+			backgroundImage: {
+				grainy: 'url("/grainy.svg")',
+			},
 			fontFamily: {
 				sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
 				mono: ["Geist Mono", ...defaultTheme.fontFamily.serif],
